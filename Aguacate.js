6ca8aeac5e0a34cca95c05711c58284e5711a2bb
Jhontwebdesign <jhont.escobar17@gmail.com>
2022-05-30 02:13:59 -0500

    Tercer commit

--- Aguacate.js
+++ Aguacate.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 Linda noche para estar estudiando
 en la oscuridad mas profunda hasta la luz mas pequeña iluminara tu camino
-=======
-asdasd
-hbknlgv
->>>>>>> 7580c42df34437e26d77443dbee31de6f5983468
